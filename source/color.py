a86961804bbb4bb74cbe4a97770f3d8cd9cfb062
Bifidy <bifidy@iOSdeMacBook-Pro.local>
2015-09-22 02:42:32 +0800

    first commit

---
+++ source/color.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from xmlhandle import xml_string, Item
+import re
+def format_color(query):
+    default_item = Item(title=u'请输入格式为 "#xxxxxx" 的颜色值',
+                        subtitle=u'结果将直接复制至粘贴板',
+                        argument=query)
+    if not validate(query):
+        print xml_string(items=[default_item])
+    rgb_tuple = rgb_color('0x' + query[1:7])
+    ui_color_item = Item(title=ui_color(rgb_tuple),
+                         subtitle='UIColor',
+                         argument=ui_color(rgb_tuple))
+    print xml_string(items=[ui_color_item])
+
+
+def rgb_color(query):
+    hexcolor = int(query,16)
+    return ((hexcolor >> 16) & 0xff, 
+            (hexcolor >> 8) & 0xff, 
+            hexcolor & 0xff)
+
+def validate(query):
+    re_str = r'^#[0-9a-fA-F]{6}$'
+    return re.match(re_str,query)
+
+def ui_color(rgb_tuple):
+    (red, green, blue) = rgb_tuple
+    _tuple = (float(red)/256, float(green)/256, float(blue)/256)
+    return '[UIColor colorWithRed:%.2f green:%.2f blue:%.2f alpha:1]' % _tuple
+
---
+++ source/xmlhandle.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from xml.dom import minidom
+
+class Item(object):
+    def __init__(self, title, subtitle, argument, 
+            icon=None, autocomplete='autocompletex', uid=None):
+        self.title = title    
+        self.subtitle = subtitle
+        self.uid = uid
+        self.argument = argument
+        self.icon = icon
+        self.autocomplete = autocomplete    
+
+    def validate(self):
+        for attr in [self.title, self.subtitle, self.argument]:
+            if not attr:
+                raise Exception, 'attrubute:%s is None' % attr.__name__
+
+def xml_string(items, first_uid=0):
+    doc = minidom.Document()
+    _items = doc.createElement('items')
+    for (index,item) in enumerate(map(__itemAdapter,items)):
+        item.setAttribute('uid', str(index+first_uid))
+        _items.appendChild(item)
+    doc.appendChild(_items)
+    return doc.toprettyxml()
+
+def __itemAdapter(item):
+    item.validate
+    doc = minidom.Document()
+    _item = doc.createElement('item')
+    _item.setAttribute('autocomplete', item.autocomplete)
+    _item.setAttribute('arg', item.argument)
+    title_attr = doc.createElement('title')
+    title_content = doc.createTextNode(item.title)
+    title_attr.appendChild(title_content)
+    subtitle_attr = doc.createElement('subtitle')
+    subtitle_content = doc.createTextNode(item.subtitle)
+    subtitle_attr.appendChild(subtitle_content)
+    _item.appendChild(title_attr)
+    _item.appendChild(subtitle_attr) 
+    return _item
+
